b138041cf9c186672ad20881e97f4cc8c3be4216
João Araujo de Souza <joao.asouz@gmail.com>
2021-10-30 17:38:18 -0300

    Bot agora responde a comandos de letras de musicas

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const { Client, Intents } = require('discord.js')
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+const { Client, Intents, Message } = require('discord.js')
 const { birthdays } = require('./birthdays.json')
 const cron = require('cron')
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS]})
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]})
 
 function prepareBirthdayMessage(userName, birthDay) {
 
@@ -27,6 +29,50 @@ client.once('ready', () => {
         prepareBirthdayMessage(birthday.name, birthdayDate)
 
     })
+})
+
+client.on("messageCreate", (msg) => {
+
+    if (msg.author.bot) return
+
+    const prefix = "$"
+    if (!msg.content.startsWith(prefix)) return
+
+    const message = msg.content.slice(1)
+    const tokens = message.split(" ")
+
+    const command = tokens.shift()
+
+
+    if (command == "letra" || command == "traducao") {
+
+        const artist = tokens.slice(0, tokens.indexOf("-")).join(" ")
+        const song = tokens.slice(tokens.indexOf("-") + 1).join(" ")
+
+        fetch(`https://api.vagalume.com.br/search.php?art=${artist}&mus=${song}`)
+            .then(response => response.json())
+            .then(result => {
+
+                if (result.type == "notfound") {
+                    msg.channel.send("Não encontrei o artista " + artist)
+        
+                } else if (result.type == "song_notfound") {
+                    msg.channel.send("Não encontrei a música " + song)
+        
+                } else if (command == "letra") {
+                    msg.channel.send(result.mus[0].text)
+                }
+
+                else if (command == "traducao") {
+                    msg.channel.send(result.mus[0].translate[0].text)
+                }
+
+            })
+            .catch(err => {
+                msg.channel.send("Deu ruim aqui -> " + err)
+            })
+    }
+
 })
 
 client.login(process.env.DISCORD_TOKEN)
