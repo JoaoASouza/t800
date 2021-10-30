24c29d538af1295383c9c3074b1afdddfed67f97
João Araujo de Souza <joao.asouz@gmail.com>
2021-10-30 13:25:08 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const { Client, Intents } = require('discord.js')
+const { birthdays } = require('./birthdays.json')
+const cron = require('cron')
+
+const token = process.env.DISCORD_TOKEN
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS]})
+
+function prepareBirthdayMessage(userName, birthDay) {
+
+    let scheduledMessage = new cron.CronJob(`00 01 00 ${birthDay.getDate() + 1} ${birthDay.getMonth()} *`, () => {
+
+        const guild = client.guilds.cache.get('759556197887639572')
+        const channel = guild.channels.cache.get('904039024300269599')
+
+        channel.send(`Parabéns, ${userName}!`)
+
+    })
+
+    scheduledMessage.start()
+}
+
+client.once('ready', () => {
+    console.log("Tudo pronto")
+
+    birthdays.forEach(birthday => {
+
+        let birthdayDate = new Date(birthday.date)
+        prepareBirthdayMessage(birthday.name, birthdayDate)
+
+    })
+})
+
+client.login(token)
